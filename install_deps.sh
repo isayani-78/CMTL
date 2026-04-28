18732426b98d95a4ed5cc75178d2ea2bbf5aa057
Sayani Maity <sayanimaity2024@gmail.com>
2026-04-28 10:08:59 +0530

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -3,7 +3,7 @@ set -e
 echo "Updating and installing system dependencies..." 
 sudo apt update 
 sudo apt install -y nmap python3-pip python3-venv libpcap-dev python3-tk 
-echo "Installing Npcap/WinPcap is required on Windows. Install Wireshark if needed."  
+echo "Installing Npcap/WinPcap is required on Windows. Install Wireshark if needed."   
 python3 -m venv venv 
 source venv/bin/activate
 pip install --upgrade pip
